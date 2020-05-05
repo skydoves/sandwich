8d71a1d322c8e5feac5c24eea6ea36e101b4cab9
skydoves <skydoves@naver.com>
2020-05-05 18:33:38 +0900

    Add DataSourceCallAdapter and DataSourceCallAdapterFactory

---
+++ sandwich/src/main/java/com/skydoves/sandwich/DataSourceCallAdapter.kt
@@ -0,0 +1,45 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.sandwich
+
+import java.lang.reflect.Type
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * DataSourceCallAdapter is an call adapter for creating [DataSource] from service method.
+ *
+ * request API network call asynchronously and returns [DataSource].
+ */
+class DataSourceCallAdapter<R> constructor(
+  private val responseType: Type
+) : CallAdapter<R, DataSource<R>> {
+
+  override fun responseType(): Type {
+    return responseType
+  }
+
+  override fun adapt(call: Call<R>): DataSource<R> {
+    val responseDataSource: ResponseDataSource<R> = ResponseDataSource()
+    return responseDataSource.combine(call, object : Callback<R> {
+      override fun onResponse(call: Call<R>, response: Response<R>) = Unit
+      override fun onFailure(call: Call<R>, t: Throwable) = Unit
+    }).request()
+  }
+}
---
+++ sandwich/src/main/java/com/skydoves/sandwich/DataSourceCallAdapterFactory.kt
@@ -0,0 +1,41 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.sandwich
+
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
+
+/**
+ * DataSourceCallAdapterFactory is an call adapter factory for creating [DataSource].
+ *
+ * Adding this class to [Retrofit] allows you to return on [DataSource] from service method.
+ *
+ * ```
+ * @GET("DisneyPosters.json")
+ * fun fetchDisneyPosterList(): DataSource<List<Poster>>
+ * ```
+ */
+class DataSourceCallAdapterFactory : CallAdapter.Factory() {
+
+  override fun get(returnType: Type, annotations: Array<Annotation>, retrofit: Retrofit): CallAdapter<*, *>? {
+    val enclosingType = returnType as ParameterizedType
+    val actualType = enclosingType.actualTypeArguments[0]
+    return DataSourceCallAdapter<Type>(actualType)
+  }
+}
