a1c03fc063ce404ec09d1ca9491bbdba4257d7da
skydoves <skydoves@naver.com>
2020-06-22 00:50:36 +0900

    Implement ApiResponseCallDelegate, CoroutinesResponseCallAdapterFactory

---
+++ sandwich/src/main/java/com/skydoves/sandwich/coroutines/ApiResponseCallDelegate.kt
@@ -0,0 +1,45 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.sandwich.coroutines
+
+import com.skydoves.sandwich.ApiResponse
+import okio.Timeout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ApiResponseCallDelegate<T>(proxy: Call<T>) : CallDelegate<T, ApiResponse<T>>(proxy) {
+
+  override fun enqueueImpl(callback: Callback<ApiResponse<T>>) = proxy.enqueue(object : Callback<T> {
+    override fun onResponse(call: Call<T>, response: Response<T>) {
+      val apiResponse = ApiResponse.of { response }
+      if (apiResponse is ApiResponse.Success) {
+        callback.onResponse(this@ApiResponseCallDelegate, Response.success(apiResponse))
+      } else if (apiResponse is ApiResponse.Failure.Error && apiResponse.errorBody != null) {
+        callback.onResponse(this@ApiResponseCallDelegate, Response.error(apiResponse.statusCode.code, apiResponse.errorBody))
+      }
+    }
+
+    override fun onFailure(call: Call<T>, t: Throwable) {
+      callback.onResponse(this@ApiResponseCallDelegate, Response.success(ApiResponse.error(t)))
+    }
+  })
+
+  override fun timeout(): Timeout = Timeout.NONE
+
+  override fun cloneImpl() = ApiResponseCallDelegate(proxy.clone())
+}
---
+++ sandwich/src/main/java/com/skydoves/sandwich/coroutines/CoroutinesResponseCallAdapterFactory.kt
@@ -0,0 +1,44 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.sandwich.coroutines
+
+import com.skydoves.sandwich.ApiResponse
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
+
+class CoroutinesResponseCallAdapterFactory : CallAdapter.Factory() {
+  override fun get(
+    returnType: Type,
+    annotations: Array<Annotation>,
+    retrofit: Retrofit
+  ) = when (getRawType(returnType)) {
+    Call::class.java -> {
+      val callType = getParameterUpperBound(0, returnType as ParameterizedType)
+      when (getRawType(callType)) {
+        ApiResponse::class.java -> {
+          val resultType = getParameterUpperBound(0, callType as ParameterizedType)
+          CoroutinesResponseCallAdapter(resultType)
+        }
+        else -> null
+      }
+    }
+    else -> null
+  }
+}
