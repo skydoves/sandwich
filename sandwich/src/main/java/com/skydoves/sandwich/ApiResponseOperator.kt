c8ba01e20fbb34e3004adea4530a068153c30055
skydoves <skydoves@naver.com>
2020-12-26 21:14:04 +0900

    Implement ApiResponseOperator and ApiResponseSuspendOperator for operating responses

---
+++ sandwich/src/main/java/com/skydoves/sandwich/ApiResponseOperator.kt
@@ -0,0 +1,47 @@
+/*
+ * Designed and developed by 2020 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.skydoves.sandwich
+
+/**
+ * ApiResponseOperator operates on an [ApiResponse] and return an [ApiResponse].
+ * This allows you to handle success and error response instead of the [ApiResponse.onSuccess],
+ * [ApiResponse.onError], [ApiResponse.onException] transformers.
+ * This operator can be applied Globally as a singleton instance, or on each [ApiResponse] one by one.
+ */
+abstract class ApiResponseOperator<T> {
+
+  /**
+   * Operates the [ApiResponse.Success] for handling successful responses if the request succeeds.
+   *
+   * @param apiResponse The successful response.
+   */
+  abstract fun onSuccess(apiResponse: ApiResponse.Success<T>)
+
+  /**
+   * Operates the [ApiResponse.Failure.Error] for handling error responses if the request failed.
+   *
+   * @param apiResponse The failed response.
+   */
+  abstract fun onError(apiResponse: ApiResponse.Failure.Error<T>)
+
+  /**
+   * Operates the [ApiResponse.Failure.Exception] for handling exception responses if the request get an exception.
+   *
+   * @param apiResponse The exception response.
+   */
+  abstract fun onException(apiResponse: ApiResponse.Failure.Exception<T>)
+}
