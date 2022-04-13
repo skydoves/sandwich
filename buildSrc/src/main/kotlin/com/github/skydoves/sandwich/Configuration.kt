705ff8c75ef328e1fa156f88f50a1685dc240191
skydoves <skydoves@naver.com>
2022-04-13 18:08:25 +0900

    Prepare the next release

--- buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Configuration.kt
@@ -23,9 +23,9 @@ object Configuration {
   const val minSdkDemo = 21
   const val majorVersion = 1
   const val minorVersion = 2
-  const val patchVersion = 3
+  const val patchVersion = 4
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 15
+  const val versionCode = 16
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
