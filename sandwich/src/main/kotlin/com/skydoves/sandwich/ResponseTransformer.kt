54541d5dd9679bd0e5fb9edf2750ad6c5fc08f69
skydoves <skydoves@naver.com>
2022-05-27 21:48:09 +0900

    Refactor onFailure/suspendOnFailure extensions to receive its instance

--- sandwich/src/main/kotlin/com/skydoves/sandwich/ResponseTransformer.kt
+++ sandwich/src/main/kotlin/com/skydoves/sandwich/ResponseTransformer.kt
@@ -340,10 +340,11 @@ public suspend inline fun <T, V> ApiResponse<T>.suspendOnSuccess(
  */
 @JvmSynthetic
 public inline fun <T> ApiResponse<T>.onFailure(
-  crossinline onResult: String.() -> Unit
+  crossinline onResult: ApiResponse.Failure<T>.() -> Unit
 ): ApiResponse<T> {
-  onError { onResult(message()) }
-  onException { onResult(message()) }
+  if (this is ApiResponse.Failure<T>) {
+    onResult(this)
+  }
   return this
 }
 
@@ -359,10 +360,11 @@ public inline fun <T> ApiResponse<T>.onFailure(
 @JvmSynthetic
 @SuspensionFunction
 public suspend inline fun <T> ApiResponse<T>.suspendOnFailure(
-  crossinline onResult: suspend String.() -> Unit
+  crossinline onResult: suspend ApiResponse.Failure<T>.() -> Unit
 ): ApiResponse<T> {
-  suspendOnError { onResult(message()) }
-  suspendOnException { onResult(message()) }
+  if (this is ApiResponse.Failure<T>) {
+    onResult(this)
+  }
   return this
 }
 
