222ed756ea33fe0aa3d5b5068f5bb72d3195589f
skydoves <skydoves@naver.com>
2022-10-14 22:16:57 +0900

    Fix wrong parameter name of ApiSuccessModelMapper

--- sandwich/src/main/kotlin/com/skydoves/sandwich/ApiSuccessModelMapper.kt
+++ sandwich/src/main/kotlin/com/skydoves/sandwich/ApiSuccessModelMapper.kt
@@ -21,15 +21,15 @@ package com.skydoves.sandwich
  *
  * A mapper interface for mapping [ApiResponse.Success] response as a custom [V] instance model.
  *
- * @see [ApiSuccessModelMapper](https://github.com/skydoves/sandwich#apierrormodelmapper)
+ * @see [ApiSuccessModelMapper](https://github.com/skydoves/sandwich#apisuccessmodelmapper)
  */
 public fun interface ApiSuccessModelMapper<T, V> {
 
   /**
    * maps the [ApiResponse.Success] to the [V] using the mapper.
    *
-   * @param apiErrorResponse The [ApiResponse.Success] error response from the network request.
+   * @param apiSuccessResponse The [ApiResponse.Success] response from the network request.
    * @return A custom [V] success response model.
    */
-  public fun map(apiErrorResponse: ApiResponse.Success<T>): V
+  public fun map(apiSuccessResponse: ApiResponse.Success<T>): V
 }
