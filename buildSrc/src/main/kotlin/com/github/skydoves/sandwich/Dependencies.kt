f1cae10963bf311939ac35adb0d360861281abb5
skydoves <skydoves@naver.com>
2023-01-13 20:40:45 +0900

    Bump Kotlin to 1.8.0

--- buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Dependencies.kt
@@ -4,7 +4,7 @@ object Versions {
   internal const val ANDROID_GRADLE_PLUGIN = "7.3.1"
   internal const val ANDROID_GRADLE_SPOTLESS = "6.7.0"
   internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
-  internal const val KOTLIN = "1.7.21"
+  internal const val KOTLIN = "1.8.0"
   internal const val KOTLIN_SERIALIZATION_JSON = "1.4.0"
   internal const val KOTLIN_GRADLE_DOKKA = "1.7.20"
   internal const val KOTLIN_BINARY_VALIDATOR = "0.12.1"
