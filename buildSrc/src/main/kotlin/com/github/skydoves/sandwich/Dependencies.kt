962f2c0643fea78d839ff4e82d0073ded3524d59
skydoves <skydoves@naver.com>
2023-05-03 21:01:28 +0900

    Bump spotless to 6.18.0

--- buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Dependencies.kt
@@ -2,7 +2,7 @@ package com.github.skydoves.sandwich
 
 object Versions {
   internal const val ANDROID_GRADLE_PLUGIN = "8.0.0-rc01"
-  internal const val ANDROID_GRADLE_SPOTLESS = "6.13.0"
+  internal const val ANDROID_GRADLE_SPOTLESS = "6.18.0"
   internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.3.0"
   internal const val KOTLIN = "1.8.10"
   internal const val KOTLIN_SERIALIZATION_JSON = "1.5.0"
