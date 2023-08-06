24050b9d4a57597fdf62dad9ae62ab7b114b336e
skydoves <skydoves@naver.com>
2023-08-06 09:59:29 +0900

    Prepare for the release 1.3.8

--- buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/sandwich/Configuration.kt
@@ -23,9 +23,9 @@ object Configuration {
   const val minSdkDemo = 21
   const val majorVersion = 1
   const val minorVersion = 3
-  const val patchVersion = 7
+  const val patchVersion = 8
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 27
+  const val versionCode = 28
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
